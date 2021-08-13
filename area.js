8952f1b5823f2704796af7ea2f14387d473e3174
gopalbbharadva <gopalbbharadva@gmail.com>
2021-08-13 12:01:55 +0530

    fix:validate inputs

--- area.js
+++ area.js
@@ -1,15 +1,19 @@
-var inputs=document.querySelectorAll('input');
-var result=document.querySelector('.formula');
-var form=document.querySelector('form');
+var inputs = document.querySelectorAll("input");
+var result = document.querySelector(".formula");
+var form = document.querySelector("form");
+var error = document.querySelector("#error");
 
-
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    console.log(inputs)
-    var base=inputs[0];
-    var height=inputs[1];
-    var area=1/2*(base.value*height.value);
-    result.textContent=area;
-    inputs[0].value=' ';
-    inputs[1].value=' ';
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  var base = inputs[0];
+  var height = inputs[1];
+  if (base.value > 0 && height.value > 0) {
+    var area = (1 / 2) * (base.value * height.value);
+    result.textContent = area;
+    inputs[0].value = " ";
+    inputs[1].value = " ";
+    error.style.display = "none";
+  } else {
+    error.innerText = "Base and height should be greater than 0";
+  }
+});
